4565cf5bfae1939df8b67adb3e6fe3a326dc0cb3
Raulx103 <raulx103@gmail.com>
2024-06-22 09:55:50 +0530

    video player added at gallerypage

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -1,30 +1,53 @@
 import { Card, CardHeader, CardBody, Button, Image } from "@nextui-org/react";
+import axios from "axios";
+import { useState } from "react";
 import { FaPlay } from "react-icons/fa";
 
 interface assetData {
+  href: string;
   links: [{ href: string }];
   data: [{ title: string; media_type: string }];
 }
 interface AssetDataProp {
   d: assetData;
 }
+
 function AssetCard(props: AssetDataProp) {
   const { d } = props;
+  const [mp4Link, setMp4Link] = useState("");
+
+  const handleClick = async () => {
+    const res = await axios.get(d.href);
+
+    // Find the first .mp4 link
+    const mp4Link = res.data.find((link: string) => link.endsWith(".mp4"));
+
+    setMp4Link(mp4Link);
+  };
   return (
     <Card className="w-[400px] min-h-[400px] ">
       <CardHeader>
-        {d.links ? (
-          <Image
-            removeWrapper
-            src={d.links[0].href}
-            className="w-[400px] h-[250px] object-cover"
-          />
+        {mp4Link ? (
+          <video width="400" height="250" controls>
+            <source src={mp4Link} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         ) : (
-          <Image
-            removeWrapper
-            className="w-[400px] h-[250px] object-contain"
-            src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1717836612/yfv3rrjmmiodj3et2pz0.png"
-          />
+          <>
+            {d.links ? (
+              <Image
+                removeWrapper
+                src={d.links[0].href}
+                className="w-[400px] h-[250px] object-cover"
+              />
+            ) : (
+              <Image
+                removeWrapper
+                className="w-[400px] h-[250px] object-contain"
+                src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1717836612/yfv3rrjmmiodj3et2pz0.png"
+              />
+            )}
+          </>
         )}
       </CardHeader>
 
@@ -35,7 +58,7 @@ function AssetCard(props: AssetDataProp) {
             Media Type : {d.data[0]?.media_type}
           </span>
           {d.data[0]?.media_type === "video" && (
-            <Button color="secondary" size="sm">
+            <Button color="secondary" size="sm" onClick={handleClick}>
               <FaPlay />
             </Button>
           )}
