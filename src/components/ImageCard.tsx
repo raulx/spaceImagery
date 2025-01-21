24bcb6f11168988185c68fca052a97e07832d320
Raulx103 <raulx103@gmail.com>
2025-01-21 14:09:39 +0530

    fixed curiosity rover http image issue with placeholder image

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,25 +1,39 @@
-import { Button, Card, CardFooter, Image } from "@nextui-org/react";
-import { GiExpand } from "react-icons/gi";
-import { handleOpenFullImage } from "../utils/functions";
+import { Button, Card, CardFooter, Image } from "@nextui-org/react"; // Importing necessary components from @nextui-org/react
+import { GiExpand } from "react-icons/gi"; // Importing expand icon from react-icons
+import { handleOpenFullImage } from "../utils/functions"; // Importing utility function to handle image expansion
 
+// ImageCard component definition
 function ImageCard(props: { imgLink: string; title: string }) {
-  const { imgLink, title } = props;
+  let { imgLink, title } = props; // Destructuring props to extract imgLink and title
+
+  // Check if the image link uses http protocol, which is blocked by the browser
+  if (imgLink.split(":")[0] === "http") {
+    // Replace http link with a placeholder image and update the title
+    imgLink =
+      "https://res.cloudinary.com/dj5yf27lr/image/upload/v1717836612/yfv3rrjmmiodj3et2pz0.png";
+    title = "Link Expired !";
+  }
+
+  // Return the Card component with the image and title
   return (
     <Card className="w-[400px] h-[440px]">
+      {/* Card component with fixed width and height */}
       <Button
         isIconOnly
         className="absolute top-2 right-2 z-10"
-        onPress={() => handleOpenFullImage(imgLink)}
+        onPress={() => handleOpenFullImage(imgLink)} // Handle button press to open full image
       >
-        <GiExpand />
+        <GiExpand /> {/* Expand icon */}
       </Button>
-
-      <CardFooter className="bottom-0 left-0 absolute z-10 bg-black  ">
-        <h1 className="text-white">{title}</h1>
+      <CardFooter className="bottom-0 left-0 absolute z-10 bg-black">
+        {/* Card footer with title */}
+        <h1 className="text-white">{title}</h1>{" "}
+        {/* Title displayed in white text */}
       </CardFooter>
       <Image
         removeWrapper
         alt="image background"
+        fallbackSrc="https://heroui.com/images/fruit-4.jpeg"
         src={imgLink}
         className="z-0 h-full w-full object-cover"
       />
@@ -27,4 +41,4 @@ function ImageCard(props: { imgLink: string; title: string }) {
   );
 }
 
-export default ImageCard;
+export default ImageCard; // Exporting the ImageCard component as default
--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,21 +1,132 @@
 import Footer from "../components/Footer";
 import NavigationBar from "../components/NavigationBar";
-import { Divider } from "@nextui-org/react";
-import MediaBox from "../components/MediaBox";
+import { Button, Divider, Radio, RadioGroup, Spinner } from "@nextui-org/react";
 import { FaSearch } from "react-icons/fa";
 import axios from "axios";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   AppDispatch,
   fetchQueryDataStart,
   fetchQueryDataError,
   fetchQueryDataSuccess,
+  RootState,
 } from "../store/store";
+import { useState } from "react";
+import AssetCard from "../components/AssetCard";
+import { typesOfMedia } from "../utils/variables";
 
 type Input = {
   searchText: string;
 };
+
+function MediaBox() {
+  const [mediaType, setmediaType] = useState<string>("All");
+  const dispatch: AppDispatch = useDispatch();
+  const { collection, metadata, links, isLoading, isError, errorMessage } =
+    useSelector((state: RootState) => {
+      return state.queryData;
+    });
+
+  const handlePageChange = async (d: {
+    rel: string;
+    prompt: string;
+    href: string;
+  }) => {
+    const queryUrl = d.href;
+    //slice the query url and make it https instead of http because http causes issue in production.
+    const slicedQueryUrl = `https://${queryUrl.slice(7)}`;
+
+    try {
+      dispatch(fetchQueryDataStart());
+
+      const res = await axios.get(slicedQueryUrl);
+
+      dispatch(fetchQueryDataSuccess(res.data.collection));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchQueryDataError("Unknown Error occured"));
+    }
+  };
+
+  let render;
+
+  const filteredData = collection.filter((d) => {
+    if (mediaType === "All") {
+      return d;
+    } else if (d.data[0].media_type === mediaType) {
+      return d;
+    }
+  });
+
+  if (isLoading) {
+    render = (
+      <div className="w-screen flex justify-center items-center min-h-96">
+        <Spinner size="lg" />
+      </div>
+    );
+  } else if (isError) {
+    render = (
+      <div className="flex justify-center items-center min-h-96">
+        <h1 className="text-2xl font-bold">{errorMessage}</h1>
+      </div>
+    );
+  } else {
+    render = (
+      <>
+        {metadata.total_hits > 0 ? (
+          <>
+            <div className="flex rounded-lg bg-red-50 sm:w-[400px] w-11/12 mx-auto border my-2 px-2 py-4 justify-center items-center ">
+              <RadioGroup
+                label="Select Media Type"
+                value={mediaType}
+                className="gap-2"
+                orientation="horizontal"
+                onValueChange={setmediaType}
+              >
+                {typesOfMedia.map((media: { key: string; value: string }) => (
+                  <Radio key={media.key} value={media.key} className="sm:mx-1">
+                    {media.value}
+                  </Radio>
+                ))}
+              </RadioGroup>
+            </div>
+            {filteredData.length > 0 ? (
+              <div className="p-4 flex gap-4 justify-center min-h-[500px] items-center flex-wrap bg-red-400">
+                {filteredData.map((d) => {
+                  return <AssetCard key={d.href} d={d} />;
+                })}
+              </div>
+            ) : (
+              <div className="text-3xl font-bold text-center my-12">
+                No Match Found For Media Type {mediaType}, Change Page !!!
+              </div>
+            )}
+
+            <div className="sm:w-1/2 mx-auto my-4 flex justify-center gap-4 items-center">
+              {links?.map((d) => {
+                if (d.prompt) {
+                  return (
+                    <Button key={d.rel} onClick={() => handlePageChange(d)}>
+                      {d.prompt}
+                    </Button>
+                  );
+                }
+              })}
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-center items-center min-h-96">
+            <h1 className="text-2xl font-bold">
+              {errorMessage || <>Search The NASA Database.</>}
+            </h1>
+          </div>
+        )}
+      </>
+    );
+  }
+  return <div>{render}</div>;
+}
 
 function GalleryPage() {
   const dispatch: AppDispatch = useDispatch();
@@ -72,7 +183,7 @@ function GalleryPage() {
             <FaSearch />
           </button>
         </form>
-        {/* <MediaSearchBox /> */}
+
         <Divider />
         <MediaBox />
       </main>
